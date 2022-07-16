404e3de4ead27118d90765c3b6fb3fef3a0dfc2d
BerNardTran <tbkhanh0908@gmail.com>
2022-07-16 10:30:45 +0700

    created API gateway

---
+++ src/main/java/com/udpt/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.udpt.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/customers/all", "/customers/login", "/customers/find")
+						.uri("http://localhost:8001"))
+				.build();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
